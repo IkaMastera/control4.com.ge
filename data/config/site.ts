ec89bfc9cd5752cba3a1766c4030d2ad0f610d8a
IkaMastera <ir.oboladze@gmail.com>
2025-12-18 15:53:18 +0400

    fix: Whatsapp Number

--- data/config/site.ts
+++ data/config/site.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  whatsappPhone: '9955XXXXXXXX', 
+  whatsappPhone: '995511223366', 
   whatsappMessage: {
-    ka: 'გამარჯობა, კონსულტაციის გავლა მსურს ჭკვიან სახლთან დაკავშირებით.',
+    ka: 'Hello! I would like to learn more about control 4!',
     en: 'Hello! I’d like to talk about Control4 solutions.',
     ru: 'Здравствуйте! Хотел(а) бы обсудить решения Control4.',
   },
