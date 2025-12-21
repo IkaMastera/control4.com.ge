b3d7798ba4a476dea65e315636064166d9d4281e
IkaMastera <ir.oboladze@gmail.com>
2025-12-21 14:36:04 +0400

    add: robots txt

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://control4.com.ge/sitemap.xml",
+  };
+}
